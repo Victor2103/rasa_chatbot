4aa9e11d56b25a8c989b44b104ab28c662add11e
Victor <vitcheffvi@cy-tech.fr>
2022-10-10 17:01:24 +0200

    fix the automation of domain creation

--- process_data/process_domain.py
+++ process_data/process_domain.py
@@ -5,16 +5,25 @@ df = pd.read_csv("topical_chat.csv", sep=",")
 count = 0
 present = []
 
-#print(df)
+# print(df)
 
 for i in df["sentiment"]:
     if i not in present:
         present.append(i)
         count += 1
 
+
+# Transform in the table the space with an underscore
+for i in range(len(present)):
+    present[i] = present[i].replace(" ", "_")
 print(present)
-        
-with open("domain.yml", "w", encoding="utf-8") as f:
+
+with open("../rasa_bot/domain.yml", "w", encoding="utf-8") as f:
     f.write('version: "3.1"\nintents: \n')
     for j in present:
-        f.write(f'  -   {j.replace(" ","_")}\n')
+        f.write(f'  - {j}\n')
+    f.write('\nresponses:\n')
+    for j in present:
+        f.write(
+            f'  utter_{j}:\n  - text: "Here is the sentiment of this : {j}."\n\n')
+    f.write("session_config:\n  session_expiration_time: 60\n  carry_over_slots_to_new_session: true")
