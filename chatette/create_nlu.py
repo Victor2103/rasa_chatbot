a21029f800b3c6d9e182a370d1af87e71d91037f
Victor <vitcheffvi@cy-tech.fr>
2022-10-24 11:43:20 +0200

    generate nlu file

--- chatette/create_nlu.py
+++ chatette/create_nlu.py
@@ -6,8 +6,16 @@ with open("output/train/output.json") as f:
 # print(data["rasa_nlu_data"]["common_examples"])
 
 # Take only the example we need.
-intents = data["rasa_nlu_data"]["common_examples"]
-print(intents[0])
+my_data = data["rasa_nlu_data"]["common_examples"]
+
+
+#Make a table with the name of the intents. 
+intents=[]
+for i in my_data:
+    if i["intent"] in intents:
+        pass
+    else:
+        intents.append(i["intent"])
 
 # Count the number of intent in the json file.
 
@@ -15,3 +23,8 @@ print(intents[0])
 with open("nlu.yml", "w", encoding="utf-8") as f:
     f.write('version: "3.1"\n')
     f.write('nlu:\n')
+    for i in intents:
+        f.write(f"- intent: {i}\n  examples: |\n")
+        for j in my_data:
+            if (j["intent"]==i):
+                f.write(f"    - {j['text']}\n")
