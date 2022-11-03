296e14978a5fabb587b0ceca34b042ecce35c2a6
Your Name <you@example.com>
2022-11-03 13:54:56 +0000

    add forms in chatbot app

--- django_app/chatbot/views.py
+++ django_app/chatbot/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+from .forms import ContactForm
+from django.views.generic.edit import FormView
 
 # Create your views here.
 
 
 def index(request):
     return render(request, template_name='index.html')
+
+
+class ContactFormView(FormView):
+    template_name = 'contact.html'
+    form_class = ContactForm
+    success_url = '/thanks/'
+
+    def form_valid(self, form):
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+        form.send_email()
+        return super().form_valid(form)
+
+
