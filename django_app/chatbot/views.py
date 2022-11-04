fe76e70fe2541868cf37679a9baad9cf6177ca8d
Your Name <you@example.com>
2022-11-04 10:29:33 +0000

    try to create the view

--- django_app/chatbot/views.py
+++ django_app/chatbot/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from .forms import ContactForm
-from django.views.generic.edit import FormView
+from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
+from .models import UserConversation
 
 import requests
 
@@ -12,15 +12,15 @@ def index(request):
     return render(request, template_name='index.html')
 
 
-class ContactFormView(FormView):
-    template_name = 'contact.html'
-    form_class = ContactForm
+class ContactFormView(CreateView):
+    model=UserConversation
+    fields=['name','message']
 
     def form_valid(self, form):
         # This method is called when valid form data has been POSTed.
         # It should return an HttpResponse.
         url = "https://a26c825a-9b57-495a-b0fd-415e0452e47f.app.gra.training.ai.cloud.ovh.net/webhooks/rest/webhook"
-        data = form.cleaned_data
+        data = form.save()
         json_data = {"sender": data['user'], "message": data["message"]}
         data = requests.post(url=url, json=json_data, headers={
             "Authorization": "Bearer ++9O7ZjOT8eEkAha1GywfOFQXnJvttgYXbmdBOxLS7sW/s4TqtdNJBVMqRav+vzO", "Content-Type": "application/json"})
