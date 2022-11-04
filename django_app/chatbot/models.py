9757108bc735c0dab6ef5b144fb694f6e7f5bc8f
Your Name <you@example.com>
2022-11-04 10:29:33 +0000

    try to create the view

--- django_app/chatbot/models.py
+++ django_app/chatbot/models.py
@@ -5,4 +5,5 @@ from django.contrib.postgres.fields import ArrayField
 
 class UserConversation(models.Model):
     name = models.CharField(max_length=30)
+    message=models.CharField(max_length=30)
     conversation=ArrayField(models.CharField(max_length=100))
