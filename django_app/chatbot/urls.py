a59d6022e040b27c82ae222208024afba1e26b9e
Victor <vitcheffvi@cy-tech.fr>
2022-11-09 12:26:24 +0100

    securise the django app

--- django_app/chatbot/urls.py
+++ django_app/chatbot/urls.py
@@ -4,7 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('discuss', views.ContactFormView.as_view(), name="discuss"),
-    path('responsebot/',
-         TemplateView.as_view(template_name="response.html"), name="reponse")
 ]
--- django_app/chatbot/views.py
+++ django_app/chatbot/views.py
@@ -31,8 +31,7 @@ class ContactFormView(CreateView):
         url = "https://a26c825a-9b57-495a-b0fd-415e0452e47f.app.gra.training.ai.cloud.ovh.net/webhooks/rest/webhook"
         data = form.save()
         json_data = {"sender": data['user'], "message": data["message"]}
-        data = requests.post(url=url, json=json_data, headers={
-            "Authorization": "Bearer ++9O7ZjOT8eEkAha1GywfOFQXnJvttgYXbmdBOxLS7sW/s4TqtdNJBVMqRav+vzO", "Content-Type": "application/json"})
+        data = requests.post(url=url, json=json_data)
         print(data.text)
         return super().form_valid(form)
 
