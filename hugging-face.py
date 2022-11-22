71d455037e9b3e701ecd7d112bfcae069eeee03e
Victor <vitcheffvi@cy-tech.fr>
2022-11-22 11:08:37 +0100

    supp token

--- hugging-face.py
+++ hugging-face.py
@@ -1,7 +1,7 @@
 import requests
 
 API_URL = "https://api-inference.huggingface.co/models/rasa/LaBSE"
-headers = {"Authorization": "Bearer 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"}
+headers = {"Authorization": "Bearer token"}
 
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
