9465552aa6a268529d8338cb9befcc1d81864a3f
Victor <vitcheffvi@cy-tech.fr>
2022-12-20 17:30:24 +0100

    update app

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,18 +1,21 @@
 from flask import Flask
 from flask import render_template
 import jwt
-from os import environ
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    encoded_jwt = jwt.encode({"user": "flask_app"},str(environ.get('SECRET_KEY')) , algorithm="HS256")
+    encoded_jwt = jwt.encode({"user": "flask_app"},str(os.getenv('SECRET_KEY')) , algorithm="HS256")
     # Create the url back end to have the rasa model train.
-    api_url = None
+    print(str(os.getenv('SECRET_KEY')))
+    api_url = os.environ.get("API_URL")
     if (api_url == None):
         api_url = 'http://localhost:5005/'
     return render_template("chatbot.html",jwebtoken=encoded_jwt,url=api_url)
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True)
